c163f81a29a00654c2aba0d03f715d432c8cac2c
Dhart1984 <donalde.hart@gmail.com>
2023-07-05 17:34:03 -0400

    push 3_bug:board not showing up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import Square from "./Components/Squares";
 
-export default function Square (){
-  return <>
-  <button className="square">x</button>
-  <button className="square">x</button>
 
-  </>
+export default function Board() {
+  return (
+    <>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
+  );
 }
-
---
+++ src/Components/Squares.jsx
@@ -0,0 +1,3 @@
+export default function Square(){
+    <button className="square">1</button>
+  }
